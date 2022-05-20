91f3e3d7cb58834d2f963e9371920ed2d1567014
Fikri Nurfadillah <fikri.nurfadillah@gtech.digital>
2022-05-20 11:10:25 +0700

    initiate blog and career page

---
+++ src/views/Blog/Blog.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="my-20 blog">
+        <h1 class="text-4xl font-bold">Coming Soon.....</h1>
+    </div>
+</template>
+
+<style scoped>
+.blog {
+    min-height: 300px;
+}
+</style>
---
+++ src/views/Career/Career.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="my-20 career">
+        <h1 class="text-4xl font-bold">Coming Soon....</h1>
+    </div>
+</template>
+
+<style scoped>
+.career{
+    min-height: 300px;
+}
+</style>
