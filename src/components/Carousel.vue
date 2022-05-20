8ec1c665a56f64a3fd0df4c2c1c344d3cdebf59d
Fikri Nurfadillah <fikri.nurfadillah@gtech.digital>
2022-05-20 11:02:27 +0700

    add button to carousel and fix image

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class=" my-20">
-      <VueSlickCarousel v-bind="settings" ref="carousel" class="w-5/6 m-auto carousel">
-        <div class="">
-          <img src="../assets/carousel-1.jpg" alt="" />
-        </div>
-        <div class="">
-          <img src="../assets/carousel-2.jpg" alt="" />
-        </div>
-        <div class="">
-          <img src="../assets/carousel-3.jpg" alt="" />
-        </div>
-      </VueSlickCarousel>
+  <div class="my-20 flex">
+    <div class="w-32 m-auto arrow-button">
+      <button @click="showPrev"><i class="fas fa-arrow-left"></i></button>
+    </div>
+    <VueSlickCarousel
+      v-bind="settings"
+      ref="carousel"
+      class="w-5/6 m-auto carousel"
+    >
+      <div class="">
+        <img src="../assets/carousel-1.jpg" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/carousel-2.jpg" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/carousel-3.jpg" alt="" />
+      </div>
+    </VueSlickCarousel>
+    <div class="w-32 m-auto arrow-button">
+      <button @click="showNext"><i class="fas fa-arrow-right"></i></button>
+    </div>
   </div>
 </template>
 
@@ -50,10 +60,18 @@ export default {
       },
     };
   },
+  methods: {
+    showNext() {
+      this.$refs.carousel.next();
+    },
+    showPrev() {
+      this.$refs.carousel.prev();
+    },
+  },
 };
 </script>
 
-<style  scoped>
+<style scoped>
 .carousel {
   width: 100%;
 }
@@ -62,14 +80,38 @@ img {
   border-radius: 32px;
   width: 95%;
 }
+button {
+  cursor: pointer;
+  width: 48px;
+  height: 48px;
+  border-radius: 50px;
+  -o-object-fit: contain;
+  object-fit: contain;
+  background: #fff;
+  outline: 0;
+  border: 0;
+  -webkit-box-shadow: 0 2px 6px 0 rgb(0 0 0 / 20%);
+  box-shadow: 0 2px 6px 0 rgb(0 0 0 / 20%);
+}
+
+button i {
+  color: #00b050;
+}
+
+.arrow-button {
+  display: none;
+}
 
 @media screen and (min-width: 769px) {
   .carousel {
     width: 80%;
   }
   img {
-    height: 490px;
+    height: 600px;
     width: 100%;
+  }
+  .arrow-button {
+    display: block;
   }
 }
 </style>
